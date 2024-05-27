fd02e1a91854b66c942c2f4929a91f9f7284c4d9
Maxim Rogozhnikov <maxim-a90@mail.ru>
2024-05-27 14:52:43 +0800

    geometryfigure

---
+++ GeometryFigure.h
@@ -0,0 +1,232 @@
+#pragma once
+#include <iostream>
+#include <cmath>
+#define M_PI 3.14159265358979323846
+#include <vector>
+#include <fstream>
+
+
+//Заготовки
+
+
+template<class T>
+class Point {
+public:
+    T x, y;
+    Point(T x = 0, T y = 0) : x(x), y(y) {}
+};
+
+template<class T>
+class GeometricFigure {
+public:
+    virtual T calc_area() = 0;
+    virtual T calc_perimeter() = 0;
+    virtual void name() = 0;
+};
+
+
+//Circle
+
+//Заголовки
+template<class T> class Circle : public GeometricFigure<T> {
+private:
+    Point<T> center;
+    T radius;
+
+public:
+    Circle(Point<T> c, T r) : center(c), radius(r) {} // Определение конструктора здесь
+
+    T calc_area() override;
+    T calc_perimeter() override;
+    void name() override;
+};
+
+//Определение
+template<class T>
+T Circle<T>::calc_area() {
+    return M_PI * radius * radius;
+}
+
+template<class T>
+T Circle<T>::calc_perimeter() {
+    return 2 * M_PI * radius;
+}
+
+template<class T>
+void Circle<T>::name() {
+    std::cout << "Circle" << std::endl;
+}
+
+
+//Ellipse
+
+//Заголовки
+template<class T>
+class Ellipse : public GeometricFigure<T> {
+private:
+    Point<T> center;
+    T a, b;
+
+public:
+    Ellipse(Point<T> c, T a, T b) : center(c), a(a), b(b) {}
+
+    T calc_area() override;
+    T calc_perimeter() override;
+    void name() override;
+};
+
+//Определения
+template<class T>
+T Ellipse<T>::calc_area() {
+    return M_PI * a * b;
+}
+
+template<class T>
+T Ellipse<T>::calc_perimeter() {
+    return M_PI * (3 * (a + b) - sqrt((3 * a + b) * (a + 3 * b)));
+}
+
+template<class T>
+void Ellipse<T>::name() {
+    std::cout << "Ellipse" << std::endl;
+}
+
+
+//Triangle
+
+//Заголовки
+template<class T>
+class Triangle : public GeometricFigure<T> {
+private:
+    Point<T> vertices[3];
+
+public:
+    Triangle(Point<T> a, Point<T> b, Point<T> c) : vertices{ a, b, c } {}
+
+    T calc_area() override;
+    T calc_perimeter() override;
+    void name() override;
+};
+
+//Определения
+template<class T>
+T Triangle<T>::calc_area() {
+    T a = sqrt(pow(vertices[1].x - vertices[0].x, 2) + pow(vertices[1].y - vertices[0].y, 2));
+    T b = sqrt(pow(vertices[2].x - vertices[1].x, 2) + pow(vertices[2].y - vertices[1].y, 2));
+    T c = sqrt(pow(vertices[0].x - vertices[2].x, 2) + pow(vertices[0].y - vertices[2].y, 2));
+    T s = (a + b + c) / 2;
+    return sqrt(s * (s - a) * (s - b) * (s - c));
+}
+
+template<class T>
+T Triangle<T>::calc_perimeter() {
+    T a = sqrt(pow(vertices[1].x - vertices[0].x, 2) + pow(vertices[1].y - vertices[0].y, 2));
+    T b = sqrt(pow(vertices[2].x - vertices[1].x, 2) + pow(vertices[2].y - vertices[1].y, 2));
+    T c = sqrt(pow(vertices[0].x - vertices[2].x, 2) + pow(vertices[0].y - vertices[2].y, 2));
+    return a + b + c;
+}
+
+template<class T>
+void Triangle<T>::name() {
+    std::cout << "Triangle" << std::endl;
+}
+
+
+//Rectangle
+
+//Заголовки
+template<class T>
+class Rectangle : public GeometricFigure<T> {
+private:
+    Point<T> vertices[4];
+
+public:
+    Rectangle(Point<T> a, Point<T> b, Point<T> c, Point<T> d) : vertices{ a, b, c, d } {}
+
+    T calc_area() override;
+    T calc_perimeter() override;
+    void name() override;
+};
+
+//Определения
+template<class T>
+T Rectangle<T>::calc_area() {
+    T width = sqrt(pow(vertices[1].x - vertices[0].x, 2) + pow(vertices[1].y - vertices[0].y, 2));
+    T height = sqrt(pow(vertices[2].x - vertices[1].x, 2) + pow(vertices[2].y - vertices[1].y, 2));
+    return width * height;
+}
+
+template<class T>
+T Rectangle<T>::calc_perimeter() {
+    T width = sqrt(pow(vertices[1].x - vertices[0].x, 2) + pow(vertices[1].y - vertices[0].y, 2));
+    T height = sqrt(pow(vertices[2].x - vertices[1].x, 2) + pow(vertices[2].y - vertices[1].y, 2));
+    return 2 * (width + height);
+}
+
+template<class T>
+void Rectangle<T>::name() {
+    std::cout << "Rectangle" << std::endl;
+}
+
+
+//Polygon
+
+//Заголовки
+template<class T>
+class Polygon : public GeometricFigure<T> {
+private:
+    std::vector<Point<T>> vertices;
+
+public:
+    Polygon(const std::string& filename);
+
+    T calc_area() override;
+    T calc_perimeter() override;
+    void name() override;
+};
+
+//Определения
+template<class T>
+//Конструктор по заданному файлу
+Polygon<T>::Polygon(const std::string& filename) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Error opening file" << std::endl;
+        return;
+    }
+    int numVertices;
+    file >> numVertices;
+    for (int i = 0; i < numVertices; ++i) {
+        T x, y;
+        file >> x >> y;
+        vertices.emplace_back(x, y);
+    }
+    file.close();
+}
+
+template<class T>
+T Polygon<T>::calc_area() {
+    T area = 0.0;
+    int n = vertices.size();
+    for (int i = 0; i < n; ++i) {
+        area += (vertices[i].x * vertices[(i + 1) % n].y) - (vertices[(i + 1) % n].x * vertices[i].y);
+    }
+    return 0.5 * std::abs(area);
+}
+
+template<class T>
+T Polygon<T>::calc_perimeter() {
+    T perimeter = 0.0;
+    int n = vertices.size();
+    for (int i = 0; i < n; ++i) {
+        perimeter += sqrt(pow(vertices[(i + 1) % n].x - vertices[i].x, 2) + pow(vertices[(i + 1) % n].y - vertices[i].y, 2));
+    }
+    return perimeter;
+}
+
+template<class T>
+void Polygon<T>::name() {
+    std::cout << "Polygon" << std::endl;
+}
+
+
---
+++ main.cpp
@@ -0,0 +1,36 @@
+﻿// Geometryfigure.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include "GeometryFigure.h"
+#include <iostream>
+
+int main() {
+    std::vector <GeometricFigure<double>*> figures;
+
+    Circle<double> circle(Point<double>(0, 0), 5.0);
+    Ellipse<double> ellipse(Point<double>(0, 0), 3.0, 2.0);
+    Triangle<double> triangle(Point<double>(0, 0), Point<double>(3, 0), Point<double>(0, 4));
+    Rectangle<double> rectangle(Point<double>(0, 0), Point<double>(3, 0), Point<double>(3, 4), Point<double>(0, 4));
+    Polygon<double> polygon("granitsy-uchastka2.txt");
+    figures.push_back(&circle);
+    figures.push_back(&ellipse);
+    figures.push_back(&triangle);
+    figures.push_back(&rectangle);
+    figures.push_back(&polygon);
+
+    for (const auto& figure : figures) {
+        figure->name();
+        std::cout << "Area: " << figure->calc_area() << std::endl;
+        std::cout << "Perimeter: " << figure->calc_perimeter() << std::endl << "\n";
+    }
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
